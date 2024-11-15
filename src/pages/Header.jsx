ca1f317817598824a0532bbe5dc4c4bea2464e00
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2024-11-16 01:28:22 +0530

    yy

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header
-      className="bg-gradient-to-r from-blue-500 to-purple-500 py-4 px-4 sm:px-6 lg:px-8 flex items-center justify-between"
+      className="bg-gradient-to-r from-blue-500 to-purple-500 py-4 px-4 sm:px-6 lg:px-8 flex items-center justify-between w-full h-24 rounded-sm" 
       style={{
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
-    >
-      <Link to="/" className="text-xl font-bold flex flex-auto justify-center align-middle border-r-amber-50 text-black shadow-lg bg-slate-400 text-center">
+      >
+      
+      <Link to="/" className="text-xl font-bold flex flex-auto justify-center align-middle border-r-amber-50 text-black shadow-lg bg-slate-400 text-center rounded-md">
         BUILD A BOOK LISTING WEB APPLICATION
       </Link>
     </header>
